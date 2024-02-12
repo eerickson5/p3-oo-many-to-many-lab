5f93000567d32ee1bdeb5c9d33583e08e2b19e5a
Elise Erickson <elisece717@gmail.com>
2024-02-12 17:30:48 -0500

    done

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,76 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
 
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        Contract.all.append(contract)
+        return contract
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in Contract.all if contract.author == self]
+    
+    def total_royalties(self):
+        total_royalties = 0
+        for contract in self.contracts():
+            total_royalties += contract.royalties
+        return total_royalties
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        
+        self.author = author
+        self.book = book
+        
+        if type(date) != str or type(royalties) != int:
+            raise Exception
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
+
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if isinstance(author, Author):
+            self._author = author
+        else:
+            raise Exception
+        
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, book):
+        if isinstance(book, Book):
+            self._book = book
+        else:
+            raise Exception
